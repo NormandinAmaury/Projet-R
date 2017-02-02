2ad9cd62dfc6e0419674d2a2456a619e970c0a41
Normandin <amau_rit@hotmail.fr>
2017-02-02 12:05:57 +0100

    Creation of a new git following an error on the previous repository that rendred the project unusable

---
+++ Projet R.R
@@ -0,0 +1,86 @@
+library(data.table)
+library(cluster)
+library(fpc)
+library(lattice)
+library(proxy)
+library(Matrix)
+library(arules)
+library(registry)
+library(recommenderlab)
+
+
+beers = fread("beer_reviews.csv")
+length(unique(beers$beer_beerid))
+summary(beers)
+beers[, length(unique(beer_beerid)), by= "review_profilename"]
+beers[review_overall<5]
+beers[,review_overall]
+head(beers[,review_overall],n=10)
+
+
+
+head(beers[,review_aroma],n=10)
+head(beers[,review_palate],n=10)
+head(beers[,review_appearance],n=10)
+head(beers[,review_taste],n=10)
+
+
+#Exercie 1
+
+Aroma = aggregate(beers$review_aroma, by=list(beers$beer_name), FUN=mean)
+Aroma10 = head(Aroma[order(-Aroma[,2], Aroma[,1]),],n=10)
+View(Aroma10)
+
+Palate = aggregate(beers$review_palate, by=list(beers$beer_name), FUN=mean)
+Palate10 = head(Palate[order(-Palate[,2], Palate[,1]),],n=10)
+View(Palate10)
+
+Appearance = aggregate(beers$review_appearance, by=list(beers$beer_name), FUN=mean)
+Appearance10 = head(Appearance[order(-Appearance[,2], Appearance[,1]),],n=10)
+View(Appearance10)
+
+Taste = aggregate(beers$review_taste, by=list(beers$beer_name), FUN=mean)
+Taste10 = head(Taste[order(-Taste[,2], Taste[,1]),],n=10)
+View(Taste10)
+
+
+#Exercice 2
+
+mysample = beers[sample(1:nrow(beers), 2000),] 
+
+mysample = na.omit(mysample)
+
+sample = mysample[,c(5,6,9,10)]
+
+sample = data.frame(lapply(sample, jitter))
+
+Cluster = kmeans(sample, 5)
+Cluster$centers
+Cluster$cluster
+
+plotcluster(sample, Cluster$cluster)
+clusplot(sample, Cluster$cluster, color=TRUE, shade=TRUE, lines=0)
+
+
+#plot(jitter(Cluster$cluster,factor=4))
+
+
+#jitter(2, factor = 1, amount = NULL)
+pairs(sample, col=c(5,6,9,10)[Cluster$cluster])
+splom(sample, col=c(5,6,9,10)[Cluster$cluster])
+
+
+
+#Exercice 3
+test = beers[,c(4,7,11)]
+
+affinity.data<-read.csv("beer_reviews.csv")
+
+affinity.data<-affinity.data[,c(4,11)]
+
+affinity.matrix<- as(affinity.data,"realRatingMatrix")
+Rec.model<-Recommender(affinity.matrix, method = "UBCF")
+recommended.items.1 <- predict(Rec.model, affinity.matrix["1",], n=5)
+as(recommended.items.1, "list")
+
+
